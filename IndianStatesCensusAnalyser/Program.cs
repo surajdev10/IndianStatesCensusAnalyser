c81e935318e4dd3a67a621716d1bd877bc40d879
surajdev10 <surajdevfb@gmail.com>
2023-01-03 20:39:07 +0530

    [suraj] Reading data from CSV file

--- IndianStatesCensusAnalyser/Program.cs
+++ IndianStatesCensusAnalyser/Program.cs
@@ -7,7 +7,7 @@ internal class Program
     public static string stateCensusCSVFilePath = @"C:\Users\suraj\source\repos\IndianStatesCensusAnalyser\IndianStatesCensusAnalyser\file\StateCensusData.csv";
     private static void Main(string[] args)
     {
-        // Console.WriteLine("Hello, World!");
+        
         CSVStateCensus cSVStateCensus = new CSVStateCensus();
         cSVStateCensus.ReadStateCensusData(stateCensusCSVFilePath);
     }
