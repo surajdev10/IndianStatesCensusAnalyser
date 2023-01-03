6d4da6d9e6695c1142b5d5366d2461d3840a4560
surajdev10 <surajdevfb@gmail.com>
2023-01-03 21:17:43 +0530

    [suraj] Read State Code data from CSV File

--- IndianStatesCensusAnalyser/Program.cs
+++ IndianStatesCensusAnalyser/Program.cs
@@ -5,10 +5,18 @@ internal class Program
 
 {
     public static string stateCensusCSVFilePath = @"C:\Users\suraj\source\repos\IndianStatesCensusAnalyser\IndianStatesCensusAnalyser\file\StateCensusData.csv";
+
+
+    public static string stateCodeFilePath = @"C:\Users\suraj\source\repos\IndianStatesCensusAnalyser\IndianStatesCensusAnalyser\file\StateCode.csv";
     private static void Main(string[] args)
     {
         
-        CSVStateCensus cSVStateCensus = new CSVStateCensus();
-        cSVStateCensus.ReadStateCensusData(stateCensusCSVFilePath);
+        //CSVStateCensus cSVStateCensus = new CSVStateCensus();
+        //cSVStateCensus.ReadStateCensusData(stateCensusCSVFilePath);
+
+
+
+         StateCode indianStateCode = new StateCode();
+        indianStateCode.ReadStateCodeData(stateCodeFilePath);
     }
 }
---
+++ IndianStatesCensusAnalyser/StateCode.cs
@@ -0,0 +1,27 @@
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndianStatesCensusAnalyser
+{
+    internal class StateCode
+    {
+
+        public void ReadStateCodeData(string filepath)
+        {
+            using (var reader = new StreamReader(filepath))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                var records = csv.GetRecords<StateCodeData>().ToList();
+                foreach (var data in records)
+                {
+                    Console.WriteLine(data.SrNo + " " + data.StateName + " " + data.Tin + " " + data.StateCodes);
+                }
+            }
+        }
+    }
+}
